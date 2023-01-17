5740f0135bbdcc8f9694459f0cf94055abb2fbcd
Andre <lasevo@gmail.com>
2023-01-17 16:58:48 -0300

    Subindo após build para android

--- sentouPagouApp/src/app/app.module.ts
+++ sentouPagouApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, {provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- sentouPagouApp/src/app/home/home.page.ts
+++ sentouPagouApp/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Produto } from '../model/produto';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   produtos = [];
+  descricao_produto: string = '';
+  vlr_produto: number = 0;
+  qtd_produto: number = 0;
+  qtd_pessoas: number = 1;
+  totalComTaxa: number = 0;
+  totalSemTaxa: number = 0;
+  taxaServico: number = 0;
+  totalComTaxaPorPessoa: number = 0;
+  totalSemTaxaPorPessoa: number = 0;
+  taxaServicoPorPessoa: number = 0;
+  produtosImprimir: Produto[] = [];
+  showItem: boolean = false;
   constructor() {}
 
+  public adicionarConsumo() {
+    let produto = new Produto();
+    produto.descricao_produto = this.descricao_produto;
+    produto.vlr_produto = this.vlr_produto;
+    produto.qtd_produto = this.qtd_produto;
+    this.produtosImprimir.push(produto);
+    this.calcularTotais();
+    console.log(this.produtosImprimir);
+  }
+
+  public calcularTotais(){
+    this.totalComTaxa = 0;
+    this.totalSemTaxa = 0;
+    this.taxaServico = 0;
+    for (let i = 0; i < this.produtosImprimir.length; i++) {
+      this.totalSemTaxa = ((this.produtosImprimir[i].vlr_produto * this.produtosImprimir[i].qtd_produto) + this.totalSemTaxa);
+      this.taxaServico = this.totalSemTaxa / 10;
+      this.totalComTaxa = this.totalSemTaxa + this.taxaServico;
+    }
+  }
+
+  public calcularTotaisPorPessoa(){
+    this.totalComTaxaPorPessoa = 0;
+    this.totalSemTaxaPorPessoa = 0;
+    this.taxaServicoPorPessoa = 0;
+    this.totalSemTaxaPorPessoa = (this.totalSemTaxa / this.qtd_pessoas);
+    this.taxaServicoPorPessoa = (this.taxaServico / this.qtd_pessoas);
+    this.totalComTaxaPorPessoa = (this.totalComTaxa / this.qtd_pessoas);
+  }
 }
+
+
